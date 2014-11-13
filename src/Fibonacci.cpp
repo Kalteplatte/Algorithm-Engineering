a65ba30c64e2900dcee7a44397ace72237390d5d
Kalteplatte <david.steffen@uni-jena.de>
2014-11-13 18:14:17 +0100

    added functions for measuring
    function to measure time works not now, because "chrono" doesn't work in
    VS 2010

--- src/Fibonacci.cpp
+++ src/Fibonacci.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <stdio.h>
 #include <math.h>
+//#include <chrono>
 #include <iostream>
 #include <assert.h>
 using namespace std;
@@ -101,14 +102,46 @@ unsigned long Fibonacci5(int n){ // function that uses only one term, which was 
 	return (unsigned long)erg;
 }
 
-unsigned long Fibonacci6(int n){
+unsigned long Fibonacci6(int n){  //Simple LookupTable for Fibonaccinumbers with open end, only limited by my own lazyness
 	if(n<=1) return n;
 	unsigned long lookup_table[]={0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170,1836311903,2971215073,4807526976,7778742049,12586269025,20365011074,32951280099,53316291173,86267571272,139583862445,225851433717,365435296162,591286729879,956722026041,1548008755920,2504730781961,4052739537881,6557470319842,10610209857723,17167680177565,27777890035288,44945570212853,72723460248141,117669030460994,190392490709135,308061521170129,498454011879264,806515533049393,1304969544928657,2111485077978050,3416454622906707,5527939700884757,8944394323791464,14472334024676221,23416728348467685,37889062373143906,61305790721611591,99194853094755497,160500643816367088,259695496911122585,420196140727489673,679891637638612258,1100087778366101931,1779979416004714189,2880067194370816120,4660046610375530309,7540113804746346429,12200160415121876738};
 	assert (n<(sizeof(lookup_table)/sizeof(lookup_table[0])));
 	return lookup_table[n];
 }
 
-int main(){
+//auto MeasureTime(int n, int i){  //Measures the time of a Fibonacci-function, which calculates the n-th Fibonaccinumber, 'i' will tell which function to use 
+//	assert(1<=i<=6);
 	
+//	auto start = chrono::high_resolution_clock::now();
+
+//    auto stop = chrono::high_resolution_clock::now();    
+//    auto elapsed = stop - start;
+//    auto us = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();  //
+//	return us;
+//}
+
+__int64 MeasureCycles ( unsigned int low, unsigned int high )   //function that measures the current CPU-cycles
+{
+		_asm
+		{
+			_emit 0x0f	// insert rtdsc opcode
+			_emit 0x31
+			mov high , edx
+			mov low , eax
+		}		
+	return ( (__int64) high << 32 ) + low;
+}
+
+int main(){
+	unsigned int high = 0;
+	unsigned int low = 0; 
+	double CycleStart = GetCPUCount ( low, high );
+ 
+	Fibonacci1(5);
+ 
+	double CycleEnd = GetCPUCount ( low, high );
+	cout << CycleStart - CycleEnd;
+	int j;
+	cin >> j;
 	return 0;
 }

c0b7e918f5d779cf7ed3043cdc3b2b7b119612e1
Kalteplatte <david.steffen@uni-jena.de>
2014-11-21 18:13:39 +0100

    prototypes for sorting

--- src/sort.cpp
+++ src/sort.cpp
@@ -15,6 +15,18 @@ bool more(double a, double b){
 	return 1;
 }
 
+
+vector <double> quicksort(vector <double> sort, void (*less)(double,double)){
+	if (sort.size()==1) return sort;
+	double index = sort[0];
+	return sort;
+}
+
+vector <double> insertionSort(vector <double> sort){
+	return sort;
+}
+
+
 int main(){
 
 	return 0;

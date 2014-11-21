f9376faa350c946665350bb36b8a9c8c8bc2dd41
Kalteplatte <david.steffen@uni-jena.de>
2014-11-21 18:13:17 +0100

    added functions to determine order

--- src/sort.cpp
+++ src/sort.cpp
@@ -5,6 +5,16 @@
 #include <assert.h>
 using namespace std;
 
+bool less(double a, double b){
+	if(a<b) return 0;
+	return 1;
+}
+
+bool more(double a, double b){
+	if(a>b) return 0;
+	return 1;
+}
+
 int main(){
 
 	return 0;

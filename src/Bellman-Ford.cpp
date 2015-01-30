ab5f57580adf0587bb539d64347c4268befdd794
Kalteplatte <david.steffen@uni-jena.de>
2015-01-30 13:11:14 +0100

    added test
    now all seems to work correct.
    next improvements are better runtime and CSR-matrix

--- src/Bellman-Ford.cpp
+++ src/Bellman-Ford.cpp
@@ -1,28 +1,20 @@
 //This algorithm gets a Graph G=(V,E) represented with a matrix. The matrix will then be changed to a matrix which elements tell us the smallest/shortest path from one point to another.
 //The difference to the Dijkstra-algorithm are the (possible) negative weights.
-//Note that the given matrix will be handled as if it's directed and has only zeroes on the diagonal. NULLs will tell us that there is no direct connection between two points.
+//Note that the given matrix will be handled as if it's directed and has only zeroes on the diagonal. Certain values will tell us that there is no direct connection between two points. (see below)
 //In the beginning every point has the distance 0 to itself, therefore the diagonal is zero.
 //Due to the fact of the negative weights, the zeroes on the diagonal may change to negative numbers in the algorithm. In this case we have at least one negative cycle inside the graph.
 #include <vector>
 #include <stdio.h>
+#include <assert.h>
 
 using namespace std;
 //TODO
 //complexity+space
-double NO=0.01; //global variable that indicates "no connection"
-double N_INF=-0.01;
 
-//checks if the given matrix represents a graph <=> matrix is square and the elements at the diagonal are 0
-//necessary? (Con: additional time for computing, especially because we go along the matrix col-wise and row-wise => bad for cache)
-/*template <typename T>
-void isGraph(vector<vector<T>> matrix){
-	assert(matrix.size()==matrix[0].size());  //check if matrix is square
-	for (int i=0;i<matrix.size();i++){
-		for(int j=i;j<matrix.size();j++){
-			if (i==j) assert(matrix[i][j]==0);   //check if diagonal=0
-		}
-	}
-}*/
+
+double NO=0.01; //global variable, which indicates "no connection"
+double N_INF=-0.01; //gloabal variable, which indicates that this point is part of a negative cycle
+
 
 //This void changes the paths from start to every other point, so that (if there exists a connection) in the end the shortest path from start to every connected point is shown.
 template <typename T>
@@ -58,20 +50,18 @@ void newGraph(vector<vector<T>>& graph){
 template <typename T>
 void makeInf(vector<vector<T>>& graph,int start){
 	if (graph[start][start]!=N_INF) return;
-	for (int i=0;i<graph.size();i++){
-		for (int j=0;j<graph.size();j++){
-			if (graph[i][j]!=NO && graph[j][i]!=NO){
-				graph[i][j]=N_INF;
-				graph[j][i]=N_INF;
-				graph[j][j]=N_INF;
-			}
-		}
+	for (int j=0;j<graph.size();j++){
+		if (graph[start][j]!=NO){
+			graph[start][j]=N_INF;
+		} 
 	}
+	
 
 
 }
 
 //This Algo checks if there exists a negative cycle in the graph, by adding the path from a point i to a point j and then backwards.
+//In addition it changes (with makeInf() ) all related points to -INF)
 template <typename T>
 bool checkNeg(vector<vector<T>>& graph){
 	bool info=1;
@@ -82,8 +72,6 @@ bool checkNeg(vector<vector<T>>& graph){
 					info=0;
 					graph[i][i]=N_INF;
 					makeInf(graph,i);
-					//TODO
-					//break? if yes change if-question (less running time for writing graph[i][i]); else change every point connected to i (including i) to number indicating the negative infinity (maybe NULL)
 				}
 			}
 		}
@@ -98,7 +86,7 @@ void Output(vector<vector<T>> graph){
 	for (int i=0;i<j;i++){
 		for (int k=0;k<j;k++){
 			if (graph[i][k]==NO) printf("%5s ","N");
-			else if (graph[i][k]==N_INF) printf ("%s ", "-INF");
+			else if (graph[i][k]==N_INF) printf ("%5s ", "-INF");
 			else	printf("%5.0f ",graph[i][k]);
 		}
 		printf("\n");
@@ -106,12 +94,34 @@ void Output(vector<vector<T>> graph){
 	printf("\n \n");
 }
 
+//simply use all the functions together
+template <typename T>
+void All(vector<vector<T>>& graph){
+	Output(graph);
+	newGraph(graph);
+	Output(graph);
+	bool info=checkNeg(graph);
+	Output(graph);
+	test(graph);
+}
+
+//tests if all -INF's have -INF as successor
+template <typename T>
+void test(vector<vector<T>> graph){
+	for (int i=0;i<graph.size();i++){
+		for (int k=0;k<graph.size();k++){
+			if (graph[i][i]==N_INF) assert(graph[i][k]==N_INF || graph[i][k]==NO);
+		}
+	}
+}
+
+
 
 int main(){
 
 	//testing purposes
 	vector <vector<double>> graph; 
-	int V = 5;
+	int V = 10;
 	graph.resize(V,vector<double>(V,NO));
 	for (int i=0;i<V;i++){
 		for (int k=0;k<V;k++){
@@ -122,12 +132,6 @@ int main(){
 				graph[i][k]=r;
 			}
 		}
-	};
-	Output(graph);
-	newGraph(graph);
-	Output(graph);
-	bool info=checkNeg(graph);
-	printf("%d\n",info);
-	Output(graph);
-	return 0;
+	}
+	All(graph);
 }
